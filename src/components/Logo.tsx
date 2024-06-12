155534825182e699e79cef7508be8c271c58c6a6
graspanta <hironi3xx9yah@proton.me>
2024-06-13 05:34:34 +0900

    add: Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,15 @@
+export const Logo = () => {
+  return (
+    <img
+      src="/static/hawk.png"
+      alt="Hawk Logo"
+      style={{
+        maxWidth: '100%',
+        width: 100,
+        display: 'flex',
+        alignItems: 'center',
+        marginLeft: 10,
+      }}
+    />
+  );
+};
